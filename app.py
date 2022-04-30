d71c4ed59e8e944a0f7f09e412e69d72fbd32c16
divyam29 <divyamjain29@gmail.com>
2022-04-30 23:34:33 +0530

    Error Handling

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from sre_constants import SUCCESS
+from black import err
+from flask import Flask, abort, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/saic'
+
 
 class Regis_form(db.Model):
     rno = db.Column(db.Integer, primary_key=True)
@@ -21,7 +24,8 @@ class Regis_form(db.Model):
 
 
 @app.route("/", methods=['GET', 'POST'])
-def home():
+def form():
+    # abort(500)
     if(request.method == 'POST'):
         rno = request.form.get('rno')
         name = request.form.get('name')
@@ -33,13 +37,31 @@ def home():
         screen_name = request.form.get('Q2')
         movie_line = request.form.get('Q3')
 
-        entry = Regis_form(rno=rno, name=name, email=email, p_rno=p_rno, p_name=p_name, p_email=p_email,
-                           talent=talent, screen_name=screen_name, movie_line=movie_line, date=datetime.now())
+        if not rno or not name or not email or not p_rno or not p_name or not p_email or not talent or not screen_name or not movie_line:
+            error_message = "All Form Fields Required"
+            return render_template('index.html')
 
-        db.session.add(entry)
-        db.session.commit()
+        else:
+            entry = Regis_form(rno=rno, name=name, email=email, p_rno=p_rno, p_name=p_name, p_email=p_email,
+                               talent=talent, screen_name=screen_name, movie_line=movie_line, date=datetime.now())
+
+            db.session.add(entry)
+            db.session.commit()
+            return render_template('success.html')
 
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    print(e)
+    return render_template('404.html')
+
+@app.errorhandler(500)
+def not_found(e):
+    db.session.rollback()
+    print(e)
+    return render_template('500.html')
 
 
 app.run(debug=True)
